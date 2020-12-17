b2745cb4ba802e35090c7c2dad8a4c5ea68b2826
yuansong6@163.com <yuansong6@163.com>
2020-12-17 11:01:31 +0800

    IRunUntillSuccess 调整

--- src/main/java/com/yuansong/tools/common/IRunUntillSuccess.java
+++ src/main/java/com/yuansong/tools/common/IRunUntillSuccess.java
@@ -2,37 +2,71 @@ package com.yuansong.tools.common;
 
 public interface IRunUntillSuccess  {
 	
+	/**
+	 * 是否开始执行
+	 * @return
+	 */
 	public boolean getPremiss();
 	
+	/**
+	 * 执行内容（报错后会继续执行）
+	 * @throws Exception
+	 */
 	public void job() throws Exception;
 	
-	default public void start() {
-		this.start(5000L);
+	/**
+	 * 执行任务前内容
+	 */
+	public void prefixJob();
+	
+	/**
+	 * 执行任务后执行
+	 */
+	public void suffixJob();
+	
+	default public void startMain() {
+		this.startMain(5000L);
 	}
 	
-	default public void start(long interval) {
+	default public void startThread() {
+		this.startThread(5000L);
+	}
+	
+	default public void startMain(long interval) {
+		runJob(interval);
+	}
+	
+	default public void startThread(long interval) {
 		Thread d = new Thread(new Runnable() {
 
 			@Override
 			public void run() {
-				while(true) {
-					try {
-						if(getPremiss()) {
-							job();
-							break;							
-						}
-					}catch(Exception e) {
-						System.out.println(ExceptionTool.getStackTrace(e));
-						e.printStackTrace();
-					}
-					try {
-						Thread.sleep(interval);
-					} catch (InterruptedException e) {}
-				}
+				runJob(interval);
 			}
 			
 		});
-		d.start();
+		d.start();		
 	}
-
+	
+	default void runJob(long interval) {
+		try {
+			this.prefixJob();
+		} catch(Exception e) {}
+		while(true) {
+			try {
+				if(getPremiss()) {
+					job();
+					break;						
+				}
+			}catch(Exception e) {
+				ExceptionTool.getStackTrace(e);
+			}
+			try {
+				Thread.sleep(interval);
+			} catch (InterruptedException e) {}
+		}
+		try {
+			this.suffixJob();
+		} catch(Exception e) {}
+	}
 }
